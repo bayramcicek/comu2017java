56d987b0eff676fe4c620c4a31c540d4e69ed3fb
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-10-04 13:15:17 +0300

    What is UML diagram

---
+++ J10whatisUML.java
@@ -0,0 +1,36 @@
+package PackageJavaNesne2;
+
+/**
+ * Created by cicek on 4.10.2017 13:07
+ */
+public class J10whatisUML {
+    public static void main(String[] args){
+
+        /*
+        The U'nified M'odeling L'anguage (UML) is a standard visual modeling language intended to be used for
+        modeling business and similar processes,
+        analysis, design, and implementation of software-based systems
+         */
+
+
+        //Birleşik Modelleme Dili - Unified Modeling Language
+
+
+        /*
+        UML’in açılımı Unified Modeling Language yani Birleşik Modelleme Dili’dir.
+
+        UML, yazılım geliştirme sürecinde projenin önemli bileşenlerini tanımlamayı, tasarlamayı ve dokümantasyonu
+        sağlayan grafik tabanlı modelleme dilidir.
+
+        Her şeyden önce UML bir programlama dili değildir, daha çok nesneye yön
+        elimli programlama (OOP) için kullanabileceğimiz bir modelleme dilidir.
+
+        Grady BOOCH, James RUMBAUGH, Ivar JACOBSON
+        tarafından geliştirilmiştir.
+
+         */
+
+
+        System.out.println("\n--SUCCESS--");
+    }
+}
