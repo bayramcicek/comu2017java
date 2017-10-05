e89d722950192e9c115d8571fa3ff9f83ca4b48b
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-10-06 00:21:00 +0300

    kilo/boy/cins

---
+++ Package003/K11.java
@@ -0,0 +1,17 @@
+package Package003;
+
+/**
+ * Created by cicek on 4.10.2017 13:29
+ */
+public class K11 {
+    public static void main(String[] args){
+
+        K12sinif s3 = new K12sinif();// default construction (atamasız,atama yapmadan)
+        K12sinif s4 = new K12sinif(100,50," aslan");
+
+
+
+
+        System.out.println("\n--SUCCESS--");
+    }
+}
