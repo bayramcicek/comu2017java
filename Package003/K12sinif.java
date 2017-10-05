24c59c0654faaf8d8a600946ed08a23d597fc1fa
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-10-06 00:21:43 +0300

    kilo/boy/cins

---
+++ Package003/K12sinif.java
@@ -0,0 +1,21 @@
+package Package003;
+
+/**
+ * Created by cicek on 5.10.2017 11:11
+ */
+public class K12sinif {
+
+    private int kilo12,boy;
+    private String cins;
+
+    public K12sinif(int a,int b,String c){
+        kilo12=a;
+        boy=b;
+        cins=c;
+
+        System.out.println("kilo = "+ kilo12 + "\nboy = " + boy + "\ncins = "+ cins);
+    }
+    public K12sinif(){
+        //boş constructor
+    }
+}
