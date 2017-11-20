aecfe57aec5e91b1cd303e0215eac66aa44a304a
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-11-21 01:15:36 +0300

    ClassShow

---
+++ Package002/ClassShow.java
@@ -0,0 +1,15 @@
+package Package002;
+
+/**
+ * Created by cicek on 21.11.2017 00:45
+ */
+ 
+ 
+public class ClassShow{
+
+    public void ShowMe(String name1){
+
+        System.out.println("\nYour lesson name is = \"" + name1 + "\"");
+
+    }
+}
