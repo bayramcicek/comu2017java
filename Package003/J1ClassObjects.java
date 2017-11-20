77158d2700180c19a57c0019a1cc3071a97e9fed
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-11-21 01:27:29 +0300

    Class/objects

---
+++ Package003/J1ClassObjects.java
@@ -0,0 +1,21 @@
+package Package003;
+
+/**
+ * Created by cicek on 2.10.2017 22:17
+ */
+
+
+public class J1ClassObjects {
+    public static void main(String[] args){
+
+        J2Kedi k1 = new J2Kedi();
+
+        System.out.println("\nBoy = " + k1.boy);
+        System.out.println("Kilo = " + J2Kedi.kilo);
+
+
+
+        System.out.println("\n--SUCCESS--");
+    }
+
+}
