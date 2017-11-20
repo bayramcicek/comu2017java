268814399d0b55733269a50d09176c34f8bd76d5
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-11-21 01:35:38 +0300

    class1

---
+++ Package004/J2Sinif.java
@@ -0,0 +1,15 @@
+package Package004;
+
+/**
+ * created by cicek on 21.11.2017 00:09
+ */
+
+
+public class J2Sinif {
+
+    public void Display1(){
+
+        System.out.println(" Now you seeing Public Display1 () method ");
+    }
+
+}
