697a7aff958a566ab1e871dfc2469a5abad3957a
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-11-21 01:36:39 +0300

    Class2

---
+++ Package004/J3Sinif.java
@@ -0,0 +1,15 @@
+package Package004;
+
+/**
+ * created by cicek on 21.11.2017 00:09
+ */
+
+
+public class J3Sinif {
+
+    public static void Display2(){
+
+        System.out.println(" Now you seeing Public Display2 () method ");
+    }
+
+}
