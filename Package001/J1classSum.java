3ca25ec83386818c3bca4c2e28a38e981cdb96b4
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-11-21 00:26:29 +0300

    Sum

---
+++ Package001/J1classSum.java
@@ -0,0 +1,17 @@
+package Package001;
+
+/**
+ * Created by cicek on 1.10.2017 22:10
+ */
+
+
+public class J1classSum {
+    public static void main(String[] args){
+
+        int a=9,b=4,toplam=0;
+        toplam=a+b;
+
+        System.out.println("Total = " + toplam);
+
+    }
+}
