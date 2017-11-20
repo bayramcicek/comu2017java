629add95defb8ad602cb8a59165300d4f14f683a
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-11-21 01:14:24 +0300

    Class/Object

---
+++ Package002/J1ClassObjects.java
@@ -0,0 +1,30 @@
+package Package002;
+
+/**
+ * Created by cicek on 2.10.2017 19:07
+ */
+
+
+import java.util.Scanner;
+
+public class J1ClassObjects {
+    public static void main(String [] args){
+
+        //klavyeden ders adı gir
+        //ders adını ShowMe() metoduna çıktı olarak göster
+
+        String lesson1;
+
+        Scanner sc1 = new Scanner(System.in); /* ekran girdisi tanımlandı*/
+
+        System.out.print(" Enter a Lesson Name =  ");
+        lesson1 = sc1.nextLine(); /* ekran girdisi alındı*/
+
+        ClassShow n1 = new ClassShow();
+        n1.ShowMe(lesson1);
+
+
+
+        System.out.println("\n--SUCCESS--");
+    }
+}
