dd5e3dd801fe8aaadde839a21ce5a2c0660a1681
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-11-21 01:28:15 +0300

    Class

---
+++ Package003/J2Kedi.java
@@ -0,0 +1,16 @@
+package Package003;
+
+/**
+ * Created by cicek on 21.11.2017 01:20
+ */
+
+
+public class J2Kedi {
+
+        int boy = 5; //instance veriable (durum değişkeni)
+
+        static int kilo = 25; // static
+
+        private String cins = "VAN"; //private sadece bulunduğu sınıfta erişilebilir haldedir
+
+}
