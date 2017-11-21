4d367bc81693e31a2c1c20e14ba5edf381948e7b
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-11-21 22:27:41 +0300

    set/get class

---
+++ Package005/J2Kedi.java
@@ -0,0 +1,22 @@
+package Package005;
+
+/**
+ * created by cicek on 21.11.2017 22:05
+ */
+
+
+public class J2Kedi {
+    private int kilo;
+
+   /* public void J2Kedi(int kilo) {
+        this.kilo = kilo;
+    }*/
+
+    public void setKilo(int k){ //set = vermek/ayarlamak  -> değer vermek
+        kilo=k;
+    }
+
+    public int getKilo() {      // get = almak  -> geri dönüş değeri almak yani return;
+        return kilo;
+    }
+}
