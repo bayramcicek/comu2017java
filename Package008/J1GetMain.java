d50145550b31283b7afa8e042dacc2345dc6031f
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-11-21 23:06:34 +0300

    Constructor

---
+++ Package008/J1GetMain.java
@@ -0,0 +1,20 @@
+package Package008;
+
+/**
+ * Created by cicek on 4.10.2017 13:29
+ */
+
+
+public class J1GetMain {
+    public static void main(String[] args){
+
+        J2get s1 = new J2get();// default construction (atamasız,atama yapmadan)
+
+        J2get s2 = new J2get(100,50," aslan");
+
+
+
+
+        System.out.println("\n--SUCCESS--");
+    }
+}
