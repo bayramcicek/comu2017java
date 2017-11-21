541d9f32f2ef155f5779524a212eaee852c5e2f5
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-11-21 22:58:02 +0300

    Class

---
+++ Package007/J2Sinifdrs.java
@@ -0,0 +1,22 @@
+package Package007;
+
+/**
+ * created by cicek on 21.11.2017 22:29
+ */
+
+
+class J2Sinifdrs {
+
+    private String courseName; // private string oluştur
+
+    public void setDersadi(String a){
+
+        courseName = a; // dışarıdan "a" ya gelen değeri ata
+
+    }
+
+    public String getDersadi() {
+
+        return courseName; // değeri döndür
+    }
+}
