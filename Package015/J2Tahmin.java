c8574451c9e2c7fc00aa5cd4a723820b70b38f1e
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-11-23 23:48:55 +0300

    Class

---
+++ Package015/J2Tahmin.java
@@ -0,0 +1,24 @@
+package Package015;
+
+/**
+ * Created by cicek on 19.10.2017 16:42
+ */
+
+
+public class J2Tahmin {
+
+    public J2Tahmin(){
+        // boş constructor
+    }
+
+    //-------------------//
+
+    public static int randSayi;
+
+    int Random(){
+
+        randSayi = (int)(Math.random()*100);
+        return randSayi;
+    }
+
+}
