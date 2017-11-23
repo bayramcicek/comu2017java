1bb04311b814122b5bdfae106c57dcf4cf0c0e7b
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-11-23 21:21:40 +0300

    Class void/static

---
+++ Package013/J2object.java
@@ -0,0 +1,32 @@
+package Package013;
+
+/**
+ * Created by cicek on 19.10.2017 16:09
+ */
+
+
+public class J2object {
+
+    public J2object(){
+
+        //yapıcı methot --> Constructor
+    }
+
+//---------------------------------------------------------------//
+
+    public void doAnything(){
+
+        System.out.println("    \ndoAnything Method  ");
+
+    }
+
+//---------------------------------------------------------------//
+
+    public static  void doStatic(){
+
+        System.out.println("    \ndoStatic Method  ");
+    }
+
+//---------------------------------------------------------------//
+
+}
