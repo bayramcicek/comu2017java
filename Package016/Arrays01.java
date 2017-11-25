cccffb08aff6c119a2f3c9447988de3199b9fe4e
Bayram ÇİÇEK <bayramcicek@outlook.com.tr>
2017-11-25 16:27:04 +0300

    Arrays introduction

---
+++ Package016/Arrays01.java
@@ -0,0 +1,36 @@
+package Package016;
+
+/**
+ * created by cicek on 25.11.2017 14:52
+ */
+
+public class Arrays01 {
+    public static void main(String[] args){
+
+        /*
+
+        Array = Dizi = 1 den fazla ve aynı tip elemanların tutulması
+
+        int[] dizi = new int[size];
+
+         */
+
+        int[] a = new int[5];//eleman sayısı --> 5
+        int i,e=15;
+
+        for (i=0;i<5;i++){
+
+            a[i] = e;
+            e=e+10;
+
+        }
+
+        System.out.println("Eleman" + "    Index" + "        Sıra");
+
+        for (i=0;i<a.length;i++){
+
+            System.out.println(a[i] + "           " + i + "           " + (i+1));
+
+        }
+    }
+}
