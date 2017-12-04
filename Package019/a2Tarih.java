580d82aa65b628ec524105ac2c1843ba14927152
Bayram ÇİÇEK <bayram@bayramcicek.com.tr>
2017-12-04 17:58:26 +0300

    30.11.2017 Lab dersindeki kodlar

---
+++ Package019/a2Tarih.java
@@ -0,0 +1,29 @@
+package Package019;
+
+/**
+ * created by cicek on 30.11.2017 15:51
+ */
+
+public class a2Tarih {
+
+    private int gun,ay,yil;
+
+    public a2Tarih(int Gun, int Ay, int Yil){
+
+        gun=Gun;
+        ay=Ay;
+        yil=Yil;
+
+        System.out.printf("%s\n",this);// aşağıdaki toString() e gider ve return değerini yazdırır.
+
+    }
+
+
+
+    public String toString() {
+
+        return String.format("%d,%d,%d",gun,ay,yil);
+
+    }
+
+}
