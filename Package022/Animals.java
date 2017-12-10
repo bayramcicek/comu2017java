5f2c0c6ac1cfc6348ba78958839df5be31764f57
Bayram ÇİÇEK <bayram@bayramcicek.com.tr>
2017-12-10 21:52:56 +0300

    KALITIM - Inheritance

---
+++ Package022/Animals.java
@@ -0,0 +1,27 @@
+package Package022;
+
+/**
+ * created by cicek on 07.12.2017 16:09
+ */
+
+public class Animals {
+
+    public Animals(){ //constructor
+
+        System.out.println("Animals created ....");
+
+    }
+
+    public void  eat(){
+
+        System.out.println("Animals eats ....");
+
+    }
+
+    public void sleep(){
+
+        System.out.println("Animals sleeps ....");
+
+    }
+
+}
