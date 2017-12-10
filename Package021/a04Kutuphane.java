f8a5420f241e61f099a6e2cd649fa0453349cf0d
Bayram ÇİÇEK <bayram@bayramcicek.com.tr>
2017-12-10 18:54:56 +0300

    Lab Quiz

---
+++ Package021/a04Kutuphane.java
@@ -0,0 +1,22 @@
+package Package021;
+
+/**
+ * created by cicek on 10.12.2017 15:01
+ */
+
+public class a04Kutuphane {
+
+    private int yil;
+
+    public a04Kutuphane(int yil){
+
+        this.yil=yil;
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("\nYılı: " + yil);
+    }
+
+}
