cc02c3d8e1bdb3a34a785c4faf84e1d7a482ac92
Bayram ÇİÇEK <bayram@bayramcicek.com.tr>
2017-12-11 15:11:50 +0300

    Class,constructor,try/catch,IllegalAccessException

--- Package020/TestMain.java
+++ Package020/TestMain.java
@@ -14,17 +14,19 @@ Girilen zamanı 24 lük ve 12 lik formatta ekrana yazdır.
 public class TestMain {
     public static void main(String[] args) throws Exception {
 
-       Time1 i1 = new Time1(); // Time1 de boş constructor gömülüdür,Nesne oluşturulabilir.
+       Time1 i1 = new Time1(); // Time1 de boş constructor oluşturuldu
 
 //        i1.setTime(50,50,50); // hata verir ve durur
-//        i1.toString();
+//        System.out.println(i1.toString());
 
 
        System.out.println("\n" + i1.toUniversalString()); // 0,0,0
+       System.out.println("---------------------------");
 
-       System.out.println("\n" + i1.toString()); // 12,0,0 am
+
+       System.out.println(i1.toString()); // 12,0,0 am  // boş constructor int değerlere 0 atadı.
        System.out.println(i1); // 12,0,0 am  // toString() e gitti (bir üstteki ile aynı)
-
+       System.out.println("---------------------------");
 
 
 //        try {
@@ -38,17 +40,24 @@ public class TestMain {
 //            System.out.println("\n" + e);
 //
 //        }
+//        System.out.println("---------------------------");
+
 
         i1.setTime(13 , 27 , 6);
-        i1.toString(); // 1,27,6 p.m.
-
-        i1.setTime(1 ,17 ,6);
-        i1.toString(); // 1,27,6 a.m.
-
-//        Time1 i1 = new Time1(11,11,11);
-//        i1.toString();
+        System.out.println(i1.toString()); // 1,27,6 p.m.
+        System.out.println("---------------------------");
 
 
+        i1.setTime(1 ,27 ,6);
+        System.out.println(i1.toString());// 1,27,6 a.m.
+        System.out.println("---------------------------");
 
+
+        Time1 i2 = new Time1(11,11,11);
+        System.out.println(i2.toString());
+        System.out.println(i2);
+
+        
     }
+
 }
