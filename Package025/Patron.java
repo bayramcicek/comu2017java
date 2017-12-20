aa4b1984781e4a5b11f1099aaa44aa61cbc825d2
Bayram ÇİÇEK <bayram@bayramcicek.com.tr>
2017-12-20 23:15:48 +0300

    Polymorphism

---
+++ Package025/Patron.java
@@ -0,0 +1,16 @@
+package Package025;
+
+/**
+ * created by cicek on 20.12.2017 20:44
+ */
+
+public class Patron extends Calisan{
+
+    @Override
+    public void method(){
+
+        System.out.println("Patron sınıfı-->Maaş: 40");
+
+    }
+
+}
