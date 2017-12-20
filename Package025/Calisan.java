6f7be0e39c5dd931d88d8e20c7ee6582d0e2165c
Bayram ÇİÇEK <bayram@bayramcicek.com.tr>
2017-12-20 23:11:18 +0300

    Polymorphism

---
+++ Package025/Calisan.java
@@ -0,0 +1,15 @@
+package Package025;
+
+/**
+ * created by cicek on 20.12.2017 20:40
+ */
+
+public class Calisan {
+
+    public void method(){
+
+        System.out.println("Calisan sınıfı-->Maaş: 20");
+
+    }
+
+}
