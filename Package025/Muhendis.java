fd44e3b0bae432b087b34901a6beab80f8ccda99
Bayram ÇİÇEK <bayram@bayramcicek.com.tr>
2017-12-20 23:15:23 +0300

    Polymorphism

---
+++ Package025/Muhendis.java
@@ -0,0 +1,16 @@
+package Package025;
+
+/**
+ * created by cicek on 20.12.2017 20:42
+ */
+
+public class Muhendis extends Calisan{
+
+    @Override
+    public void method(){
+
+        System.out.println("Muhendis sınıfı-->Maaş: 35");
+
+    }
+
+}
